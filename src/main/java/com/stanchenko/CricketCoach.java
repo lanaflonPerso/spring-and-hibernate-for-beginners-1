8d15ab5601d246892a7804f2d59d03603aca38f3
Serhii Tanchenko <serg.tanchenko@gmail.com>
2019-10-20 20:53:17 +0300

    added few more fields

--- src/main/java/com/stanchenko/CricketCoach.java
+++ src/main/java/com/stanchenko/CricketCoach.java
@@ -4,13 +4,20 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
-    public CricketCoach() {
-        System.out.println("CricketCoach: Private constructor is called");
-    }
+    private String email;
+
+    private String team;
 
     public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("CricketCoach: setFortuneService is called");
         this.fortuneService = fortuneService;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
     }
 
     public String getDailyWorkout() {
